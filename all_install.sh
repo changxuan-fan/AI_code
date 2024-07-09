dd9a9fdae251b4b82ae73070d95d4758a99d1eb0
Changxuan Fan <fanchx@bu.edu>
2024-07-09 14:13:23 -0500

    Use pip instead of conda

--- all_install.sh
+++ all_install.sh
@@ -9,31 +9,22 @@ git clone https://github.com/changxuan-fan/Real-ESRGAN.git
 git clone https://github.com/changxuan-fan/PaddleOCR
 
 
-# Initialize conda
-curl -LO https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh -b -p /workspace/miniconda3
-echo -e "\n# >>> conda initialize >>>\n# !! Contents within this block are managed by 'conda init' !!\n__conda_setup=\"\$('/workspace/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)\"\nif [ \$? -eq 0 ]; then\n    eval \"\$__conda_setup\"\nelse\n    if [ -f \"/workspace/miniconda3/etc/profile.d/conda.sh\" ]; then\n        . \"/workspace/miniconda3/etc/profile.d/conda.sh\"\n    else\n        export PATH=\"/workspace/miniconda3/bin:\$PATH\"\n    fi\nfi\nunset __conda_setup\n# <<< conda initialize <<<\n" >> ~/.bashrc
-source ~/.bashrc
-conda update -y conda
-
-
-# Create environments
-conda create -y -n facefusion_env python=3.10
-conda create -y -n propainter_env python=3.10
-conda create -y -n esrgan_env python=3.10
-conda create -y -n paddle_env python=3.10
-
 # Update package lists and install APT packages
 apt-get update -y
-apt-get install -y  ffmpeg mesa-va-drivers libcublaslt11 libcublas11 libcufft10 cuda-cudart-11-8 libcudnn8
+apt-get install -y ffmpeg mesa-va-drivers libcublaslt11 libcublas11 libcufft10 cuda-cudart-11-8 libcudnn8 python3-venv python3-pip
+
+# Create virtual environments
+python3 -m venv /workspace/facefusion_env
+python3 -m venv /workspace/propainter_env
+python3 -m venv /workspace/esrgan_env
+python3 -m venv /workspace/paddle_env
 
 # Function to set up FaceFusion environment
 setup_facefusion() {
-  source ~/.bashrc
-  conda init
+  source /workspace/facefusion_env/bin/activate
   cd /workspace/facefusion
-  conda activate facefusion_env
-  conda install -y conda-forge::openvino=2023.1.0
+  pip install --upgrade pip
+  pip install openvino==2023.1.0
   pip install -r requirements.txt
   python run.py &
   bg_pid=$!
@@ -46,43 +37,41 @@ setup_facefusion() {
   mv facefusion_rename.ini facefusion.ini
   sleep 120
   kill $bg_pid
-  conda deactivate
+  deactivate
   cd /workspace
 }
 
 # Function to set up ProPainter environment
 setup_propainter() {
+  source /workspace/propainter_env/bin/activate
   cd /workspace/ProPainter
-  conda activate propainter_env
+  pip install --upgrade pip
   pip install -r requirements.txt
-
-  conda deactivate
+  deactivate
   cd /workspace
 }
 
 # Function to set up Real-ESRGAN environment
 setup_esrgan() {
-  source ~/.bashrc
-  conda init
+  source /workspace/esrgan_env/bin/activate
   cd /workspace/Real-ESRGAN
-  conda activate esrgan_env
-  pip install basicsr facexlib gfpgan 
+  pip install --upgrade pip
+  pip install basicsr facexlib gfpgan
   pip install -r requirements.txt
   python setup.py develop
-  cp degradations.py /workspace/miniconda3/envs/esrgan_env/lib/python3.10/site-packages/basicsr/data/degradations.py
-  conda deactivate
+  cp degradations.py /workspace/facefusion_env/lib/python3.10/site-packages/basicsr/data/degradations.py
+  deactivate
   cd /workspace
 }
 
 # Function to set up PaddleOCR environment
 setup_paddleocr() {
-  source ~/.bashrc
-  conda init
+  source /workspace/paddle_env/bin/activate
   cd /workspace/PaddleOCR
-  conda activate paddle_env
-  pip install paddlepaddle-gpu==2.6.1 -f https://www.paddlepaddle.org.cn/whl/linux/cudnnin/stable.html --no-index --no-deps &
-  pip install -r requirements.txt 
-  conda deactivate
+  pip install --upgrade pip
+  pip install paddlepaddle-gpu==2.6.1 -f https://www.paddlepaddle.org.cn/whl/linux/cudnnin/stable.html --no-index --no-deps
+  pip install -r requirements.txt
+  deactivate
   cd /workspace
 }
 
@@ -98,3 +87,4 @@ end_time=$(date +%s)
 execution_time=$((end_time - start_time))
 
 echo "All environments have been set up in $execution_time seconds."
+
--- all_start.sh
+++ all_start.sh
@@ -33,30 +33,30 @@ GPU_TRACK_PID=$!
 chmod +x /workspace/AI_code/*
 mkdir -p /workspace/results
 
-conda activate facefusion_env
+source /workspace/facefusion_env/bin/activate
 cd /workspace/facefusion
-track_time /workspace/AI_code/facefusion_batch.sh  -f /workspace/facefusion/face_food_eating.webp -i /workspace/facefusion/inputs -o /workspace/results/swapped
+track_time /workspace/AI_code/facefusion_batch.sh -f /workspace/facefusion/face_food_eating.webp -i /workspace/facefusion/inputs -o /workspace/results/swapped
 track_time /workspace/AI_code/extract_frames.sh -i /workspace/results/swapped -o /workspace/results/frames
-conda deactivate
+deactivate
 
-conda activate paddle_env
+source /workspace/paddle_env/bin/activate
 cd /workspace/PaddleOCR
 track_time /workspace/AI_code/paddle_batch.sh --parent-input-dir /workspace/results/frames --parent-output-dir /workspace/results/frames-mask --detected-text-dir /workspace/results/detected_text
 track_time /workspace/AI_code/split_dir.sh --parent-dir /workspace/results/frames --files-per-dir 600
 track_time /workspace/AI_code/split_dir.sh --parent-dir /workspace/results/frames-mask --files-per-dir 600
-conda deactivate
+deactivate
 
-conda activate propainter_env
+source /workspace/propainter_env/bin/activate
 cd /workspace/ProPainter
-track_time /workspace/AI_code/propainter_batch.sh -v /workspace/results/frames  -m /workspace/results/frames-mask -o /workspace/results/propainted
+track_time /workspace/AI_code/propainter_batch.sh -v /workspace/results/frames -m /workspace/results/frames-mask -o /workspace/results/propainted
 track_time /workspace/AI_code/extract_propainter.sh -i /workspace/results/propainted -o /workspace/results/propaint_extracted
 track_time /workspace/AI_code/combine_videos.sh -p /workspace/results/propaint_extracted -o /workspace/results/propaint_combined -t /workspace/results/file_list.txt
-conda deactivate
+deactivate
 
-conda activate esrgan_env
+source /workspace/esrgan_env/bin/activate
 cd /workspace/Real-ESRGAN
 track_time /workspace/AI_code/esrgan_batch.sh -i /workspace/results/propaint_combined -o results /workspace/results/HD
-conda deactivate
+deactivate
 
 # Stop tracking GPU memory usage
 kill $GPU_TRACK_PID
